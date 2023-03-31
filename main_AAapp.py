c7a4cc1e0d4a6ed3388e200e1d9155ed1b41b1d5
kah221 <hbk.kajioka@gmail.com>
2023-04-01 07:49:10 +0900

    forth commit

--- main_AAapp.py
+++ main_AAapp.py
@@ -3,5 +3,5 @@ from PIL import Image
 st.title('エイプリルフールです')
 st.caption('突貫3時間クオリティ...')
 
-image = Image.open('./data/haxa.png')
+image = Image.open('./data/haxa-.png')
 st.image(image, width=300)
